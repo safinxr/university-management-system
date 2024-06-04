ec507de533f908dd5db1cf1807032a3ae78c0b02
safinxr <safinxr@gmail.com>
2024-06-04 12:59:19 +0600

    generateStudentId modified

--- src/app/modules/users/user.utils.ts
+++ src/app/modules/users/user.utils.ts
@@ -16,12 +16,23 @@ const findLastStudentId = async () => {
     })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
-   let incrementId = (Number(currentId) + 1).toString().padStart(4, "0");
+  let currentId = (0).toString();
+  const lastStudent = await findLastStudentId(); 
+  const lastStudentSemesterYear = lastStudent?.substring(0, 4);
+  const lastStudentSemesterCode = lastStudent?.substring(4, 6);
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+
+  if(lastStudent && lastStudentSemesterCode === currentSemesterCode && lastStudentSemesterYear === currentYear){
+    currentId = lastStudent.substring(6);
+  }
+
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, "0");
   incrementId = `${payload.year}${payload.code}${incrementId}`;
   return incrementId;
 };
